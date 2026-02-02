d250616410d1e622784c76f7e620b8664e3791be
LoejeeMan <ldulaugon123@gmail.com>
2026-02-02 19:16:12 +0800

    feat: Added roles for each page in sitemap

--- lib/shared/data/sitemap_items.dart
+++ lib/shared/data/sitemap_items.dart
@@ -1,40 +1,52 @@
 import 'package:printsari_sia/shared/types/types.dart';
 
 const siteMapItems = <SiteMapItem>[
-  SiteMapItem(title: 'Dashboard', iconPath: 'assets/house.svg', path: '/'),
+  SiteMapItem(
+    title: 'Dashboard',
+    iconPath: 'assets/house.svg',
+    path: '/',
+    roles: [Role.cashier, Role.admin],
+  ),
   SiteMapItem(
     title: 'POS Terminal',
     iconPath: 'assets/shopping-cart.svg',
     path: '/pos',
+    roles: [Role.cashier, Role.admin],
   ),
   SiteMapItem(
     title: 'Activity Log',
     iconPath: 'assets/clipboard-list.svg',
     path: '/activity-log',
+    roles: [Role.cashier, Role.admin],
   ),
   SiteMapItem(
     title: 'Inventory',
     iconPath: 'assets/package.svg',
     path: '/inventory',
+    roles: [Role.cashier, Role.admin],
   ),
   SiteMapItem(
     title: 'Financial Reports',
     iconPath: 'assets/chart-column.svg',
     path: '/financial-reports',
+    roles: [Role.admin],
   ),
   SiteMapItem(
     title: 'Products & Services',
     iconPath: 'assets/settings.svg',
     path: '/products-services',
+    roles: [Role.admin],
   ),
   SiteMapItem(
     title: 'Business Expenses',
     iconPath: 'assets/dollar-sign.svg',
     path: '/expenses',
+    roles: [Role.admin],
   ),
   SiteMapItem(
     title: 'User Management',
     iconPath: 'assets/users.svg',
     path: '/users',
+    roles: [Role.admin],
   ),
 ];
--- lib/shared/types/sitemap_item.dart
+++ lib/shared/types/sitemap_item.dart
@@ -1,11 +1,15 @@
+import 'package:printsari_sia/shared/types/role.dart';
+
 class SiteMapItem {
   final String title;
   final String iconPath;
   final String path;
+  final List<Role> roles;
 
   const SiteMapItem({
     required this.title,
     required this.iconPath,
     required this.path,
+    required this.roles,
   });
 }
